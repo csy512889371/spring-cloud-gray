a013829d8f48d5ce07e9839c621668a339f75183
chenshiying <chensy@timeloit.com>
2020-09-22 14:42:55 +0800

    [修改] loit本地化修改

--- spring-cloud-gray-plugins/spring-cloud-gray-plugin-ribbon-nacos-discovery/src/main/java/cn/springcloud/gray/client/plugin/ribbon/nacos/configuration/GrayClientRibbonNacosAutoConfiguration.java
+++ spring-cloud-gray-plugins/spring-cloud-gray-plugin-ribbon-nacos-discovery/src/main/java/cn/springcloud/gray/client/plugin/ribbon/nacos/configuration/GrayClientRibbonNacosAutoConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ConditionalOnProperty(value = "gray.enabled")
@@ -29,6 +30,7 @@ public class GrayClientRibbonNacosAutoConfiguration {
 
 
     @Bean
+    @Primary
     public ServerExplainer<Server> serverExplainer(
             SpringClientFactory springClientFactory, VersionExtractor<Server> versionExtractor) {
         return new NacosServerExplainer(springClientFactory, versionExtractor);
